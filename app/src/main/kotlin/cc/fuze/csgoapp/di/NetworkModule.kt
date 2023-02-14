250143f70c6320d1564ff7aa5b0021bae37b10c6
Hugo Rossi <hugo.hrossi@gmail.com>
2023-02-14 14:27:14 -0300

    Add GsonConverterFactory

--- app/src/main/kotlin/cc/fuze/csgoapp/di/NetworkModule.kt
+++ app/src/main/kotlin/cc/fuze/csgoapp/di/NetworkModule.kt
@@ -1,18 +1,21 @@
 package cc.fuze.csgoapp.di
 
 import cc.fuze.csgoapp.BuildConfig
+import com.google.gson.Gson
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import okhttp3.logging.HttpLoggingInterceptor.*
 import okhttp3.logging.HttpLoggingInterceptor.Level.NONE
 import org.koin.dsl.module
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val networkModule = module {
 
     single {
         Retrofit.Builder()
             .baseUrl("https://api.pandascore.co") // TODO - Extract
+            .addConverterFactory(GsonConverterFactory.create())
             .client(get())
             .build()
     }
