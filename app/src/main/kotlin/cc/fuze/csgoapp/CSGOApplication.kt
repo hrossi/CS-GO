13451680190778ef79ff95c8aafb9014fe418bd1
Hugo Rossi <hugo.hrossi@gmail.com>
2023-02-14 14:14:51 -0300

    Add Koin

--- app/src/main/kotlin/cc/fuze/csgoapp/CSGOApplication.kt
+++ app/src/main/kotlin/cc/fuze/csgoapp/CSGOApplication.kt
@@ -2,13 +2,27 @@ package cc.fuze.csgoapp
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 class CSGOApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
+        startKoin()
         forceDarkMode()
+    }
+
+    private fun startKoin() {
+        startKoin {
+            androidLogger()
+            androidContext(this@CSGOApplication)
+            modules(
+                listOf()
+            )
+        }
     }
 
     private fun forceDarkMode() {
